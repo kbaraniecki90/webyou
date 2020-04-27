b3b67cb6863290114ae04e0d5b68f4a145e52fbf
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2020-04-27 22:11:41 +0200

    start

---
+++ wp-content/themes/webyou/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ * @link https://wwww.webuoy.pl
+ *
+ * @package WebYou
+ * @subpackage WebYou
+ * @since WebYou
+ */
+
+?>
+
+hello 1234abcacdazzz
---
+++ wp-content/themes/webyou/src/js/main.js
@@ -0,0 +1,1 @@
+console.log('dupa123101123 klop')
